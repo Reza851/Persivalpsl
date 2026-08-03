b85c41b1ce76101e3bc806bb68e5a4aa5610fa7e
Reza <rezamortazaei1395@gmail.com>
2026-08-03 07:53:20 +0330

    update bot code

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Bot is ready")
+
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام، ربات فعال است ✅")
+
+token = os.getenv("BOT_TOKEN")
+
+app = Application.builder().token(token).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
